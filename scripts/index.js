1c8aa32923dcecc84dcd2061883a8a09b07f9aee
soumik9 <soumik.ahammed.9@gmail.com>
2023-07-27 15:16:51 +0600

    design inputs and get customer and details from input

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,42 @@
-console.log('hi');
+const inputFields = {
+    date: {
+        input: document.getElementById("date"),
+        display: document.getElementById("dateDisplay")
+    },
+    customerName: {
+        input: document.getElementById("customerName"),
+        display: document.getElementById("customerNameDisplay")
+    },
+    customerMobile: {
+        input: document.getElementById("customerMobile"),
+        display: document.getElementById("customerMobileDisplay")
+    },
+    customerAddress: {
+        input: document.getElementById("customerAddress"),
+        display: document.getElementById("customerAddressDisplay")
+    },
+    attention: {
+        input: document.getElementById("attention"),
+        display: document.getElementById("attentionDisplay")
+    },
+    salePerson: {
+        input: document.getElementById("salePerson"),
+        display: document.getElementById("salePersonDisplay")
+    },
+    creditLimit: {
+        input: document.getElementById("creditLimit"),
+        display: document.getElementById("creditLimitDisplay")
+    },
+
+};
+
+function updateDisplay(inputField) {
+    inputField.display.textContent = inputField.input.value;
+}
+
+// Attach event listeners to each input field
+for (const fieldKey in inputFields) {
+    const field = inputFields[fieldKey];
+    field.input.addEventListener("input", () => updateDisplay(field));
+}
+
